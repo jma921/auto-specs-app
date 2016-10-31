ed75c077b5ba97fb9ee0337a837393e16ac426e7
John Albritton <jma921@gmail.com>
2016-10-30 23:04:52 -0500

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App pt-3">
+      <div className="App">
+        <Navbar />
         <div className="container-fluid">
           {this.props.children}
         </div>
