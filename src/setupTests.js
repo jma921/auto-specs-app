1975b1f16e98ecca99d411984b32173ef739cdbf
John Albritton <jma921@gmail.com>
2016-10-31 19:43:17 -0500

    Remove mocha tests

--- src/setupTests.js
+++
@@ -1,9 +0,0 @@
-const localStorageMock = {
-  Tooltip: jest.fn(),
-  reactstrap: jest.fn(),
-  base: jest.fn(),
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  clear: jest.fn()
-};
-global.localStorage = localStorageMock
