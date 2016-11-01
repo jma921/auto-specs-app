b5caf2bc54c24eec8fbbb8a2010e7bf3979b4745
John Albritton <jma921@gmail.com>
2016-10-31 23:00:57 -0500

    Fix missing base.js file

--- src/Home.test.js
+++ src/Home.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Home from './Home';
 
+const base = jest.fn();
+
 it('renders without crashing', () => {
   shallow(<Home />);
 });
