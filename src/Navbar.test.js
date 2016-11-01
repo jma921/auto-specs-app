c4f5614ff00bad9bb2dd7cac3efa8387cf868da3
John Albritton <jma921@gmail.com>
2016-10-31 19:00:04 -0500

    Fix CI integration and test suite

--- src/Navbar.test.js
+++ src/Navbar.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import Navbar from './Navbar';
 
 it('renders without crashing', () => {
   shallow(<Navbar />);
 });
+
--- src/Specifications.test.js
+++ src/Specifications.test.js
@@ -2,38 +2,34 @@ import sinon from 'sinon';
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import Clipboard from 'clipboard';
-jest.unmock('Tooltip');
-import { Tooltip } from 'reactstrap';
 import Specifications from './Specifications';
 
 const specs = {
     "oilCap": "5.3",    
 };
 
-const wrapper = mount(<Specifications make="Acura" model="ILX" year="2013" engine="2.0" data={specs} />);
+const wrapper = shallow(<Specifications make="Acura" model="ILX" year="2013" engine="2.0" data={specs} />);
 
 it('renders without crashing', () => {
   shallow(<Specifications make="Acura" model="ILX" year="2013" engine="2.0" data={specs} />);
 });
 
 it('make should be Acura', () => {
-//   const wrapper = mount(<Specifications make="Acura" model="ILX" year="2013" engine="2.0" data={specs} />);
-  expect(wrapper.props().make).toBe("Acura");
+  expect(wrapper.instance().props.make).toBe("Acura");
 });
 
 it('model should be ILX', () => {
-//   const wrapper = mount(<Specifications make="Acura" model="ILX" year="2013" engine="2.0" data={specs} />);
-  expect(wrapper.props().model).toBe("ILX");
+  expect(wrapper.instance().props.model).toBe("ILX");
 });
 
 it('year should be 2013', () => {  
-  expect(wrapper.props().year).toBe("2013");
+  expect(wrapper.instance().props.year).toBe("2013");
 });
 
 it('engine should be 2.0', () => {  
-  expect(wrapper.props().engine).toBe("2.0");
+  expect(wrapper.instance().props.engine).toBe("2.0");
 });
 
 it('data.oilCap should be 5.3', () => {  
-  expect(wrapper.props().data.oilCap).toBe("5.3");
+  expect(wrapper.instance().props.data.oilCap).toBe("5.3");
 });
