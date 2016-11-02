dc9875c8676a1dd3c7f41e603afd7806699f51d1
John Albritton <jma921@gmail.com>
2016-11-01 23:48:35 -0500

    Fixed Home test

--- src/Home.test.js
+++ src/Home.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-// import Home from './Home';
-
+import Home from './Home';
 
 
 it('renders without crashing', () => {
-  console.log('pass');
+  shallow(<Home />);
 });
